79b48afd10761b8352fbfc45b816881183562aba
Eldhose K A <eldhoseka.abraham@gmail.com>
2020-06-13 19:51:07 +0530

    Server Redirection Added

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  req.session.sid = "Hi Socket!...";
-  res.render('index', { title: 'Express' });
+  //req.session.sid = "Hi Socket!...";
+  //res.render('index', { title: 'Express' });
+  res.redirect('/pageone.html');
 });
 
 module.exports = router;
