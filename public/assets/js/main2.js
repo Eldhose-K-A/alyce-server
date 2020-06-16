e9aa9f6195753decc63a7f469df3fc856de97923
Eldhose K A <eldhoseka.abraham@gmail.com>
2020-06-16 12:28:22 +0530

    Template Completed

--- public/assets/js/main2.js
+++ public/assets/js/main2.js
@@ -52,8 +52,8 @@
         class: 'mobile-nav d-lg-none'
       });
       $('body').append($mobile_nav);
-      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-      $('body').append('<div class="mobile-nav-overly"></div>');
+      //$('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+      //$('body').append('<div class="mobile-nav-overly"></div>');
   
       $(document).on('click', '.mobile-nav-toggle', function(e) {
         $('body').toggleClass('mobile-nav-active');
